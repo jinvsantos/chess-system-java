7b5c7e2a482aca888c266da28d6dabf8c8b9203f
Jean <jeanvictor.san@gmail.com>
2023-09-22 18:34:33 -0300

    Começando a implementação de Tabuleiro e Peça

---
+++ src/boardgame/Piece.java
@@ -0,0 +1,19 @@
+package boardgame;
+
+public class Piece {
+	
+	protected Position positon;
+	private Board board;
+	
+	public Piece(Board board) {
+		this.board = board;
+		positon = null;
+
+	}
+
+	protected Board getBoard() {
+		return board;
+	}
+	
+	
+}
