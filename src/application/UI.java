fedcc859780d1906b7f5c11fd31ea63f62bb2529
Jean <jeanvictor.san@gmail.com>
2023-09-28 19:49:08 -0300

    Queen

--- src/application/UI.java
+++ src/application/UI.java
@@ -78,7 +78,7 @@ public class UI {
 			}
 			System.out.println();
 		}
-		System.out.println("  A B C D E F G H");
+		System.out.println("  a b c d e f g h");
 	}
 
 	public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
@@ -89,7 +89,7 @@ public class UI {
 			}
 			System.out.println();
 		}
-		System.out.println("  A B C D E F G H");
+		System.out.println("  a b c d e f g h");
 	}
 	private static void printPiece(ChessPiece piece, boolean background) {
 		if(background) {
